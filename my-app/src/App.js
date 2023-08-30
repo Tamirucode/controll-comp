6bca52f791992691d8775523e1a209fe690dd6f0
Tamirucode <tam.egu76@gmail.com>
2023-08-30 00:46:42 +0000

    add name search filter to my app

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import { SearchBar3 } from "./components/SearchBar3";
+
+function App() {
+  return <div className="App">
+      <SearchBar3 />
+  </div>;
+}
+
+export default App;
